2ee1ba5e1564109a2ea67d7188b4482fa388f1c1
ayodeji oni <ayodejioni1505@gmail.com>
2024-05-27 07:01:31 +0100

    python: week 2 assignment

---
+++ python/list_assignment.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+new_list = [50, 60, 70]
+
+my_list.extend(new_list)
+
+del my_list[-1]
+
+my_list.sort()
+
+print("Index of 30:", my_list.index(30))
